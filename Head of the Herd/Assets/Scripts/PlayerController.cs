21fe33d883ae704d82dec49666750075a97066a9
Joanna Oluoch <111391937+astronaut-nemo@users.noreply.github.com>
2022-11-09 23:58:57 +0300

    Joystick Player Control
    -Added player movement (without rotation) with joystick
    -Added materials of base colours for the primitives

--- Head of the Herd/Assets/Scripts/PlayerController.cs
+++ Head of the Herd/Assets/Scripts/PlayerController.cs
@@ -2,8 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+ // Add Rigidbody components and prevent them from being deleted
+[RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
+
 public class PlayerController : MonoBehaviour
 {
+    // References
+    [SerializeField] private Rigidbody playerRb; // Holds ref. to player Rigidbody component
+    [SerializeField] private FixedJoystick joystick; // Holds ref. to fixed joystick component
+
+    // Variables
+    [SerializeField] private float moveSpeed; // Holds player speed variable
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        playerRb.velocity = new Vector3(joystick.Horizontal * moveSpeed, playerRb.velocity.y, joystick.Vertical * moveSpeed);  // Input from the joystick controls the player movement in x- and z-axes
     }
 }
