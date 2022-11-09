5bdf754d069672a8676227facbd01b687bea00a4
Joanna Oluoch <111391937+astronaut-nemo@users.noreply.github.com>
2022-11-10 00:41:32 +0300

    Joystick Player Controller
    -Added rotation to the player control to have player face direction of turning

--- Head of the Herd/Assets/Scripts/PlayerController.cs
+++ Head of the Herd/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     // References
     [SerializeField] private Rigidbody playerRb; // Holds ref. to player Rigidbody component
     [SerializeField] private FixedJoystick joystick; // Holds ref. to fixed joystick component
+    // [SerializedField] private Animator playerAnimator; // Holds ref. to player Animator component
 
     // Variables
     [SerializeField] private float moveSpeed; // Holds player speed variable
@@ -24,5 +25,12 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         playerRb.velocity = new Vector3(joystick.Horizontal * moveSpeed, playerRb.velocity.y, joystick.Vertical * moveSpeed);  // Input from the joystick controls the player movement in x- and z-axes
+
+        // Player faces in direction they are turning
+        if(joystick.Horizontal != 0 || joystick.Vertical != 0) // If the player is turning based on joystick input, then...
+        {
+            transform.rotation = Quaternion.LookRotation(playerRb.velocity); // Sets player forward in direction playerRb is facing
+            // playerAnimator.Set-("isRunning", true); // Set player animation to running animation
+        } // else {playerAnimator.Set-("isRunning", false); // Set player animation to idle animation}
     }
 }
