f45e1550ff55f21a85d22d949d749b83ac3ba61b
Joanna Oluoch <111391937+astronaut-nemo@users.noreply.github.com>
2022-11-11 01:00:58 +0300

    Shooting Mechanic
    -Projectiles are destroyed after some distance

---
+++ Head of the Herd/Assets/Scripts/Projectile.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    // References
+    [SerializeField] private Vector3 firingPoint;
+    
+    // Variables
+    [SerializeField] private float maxProjectileDistance;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        firingPoint = transform.position; // Save the position at which the projectile was spawned
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Destroy fired projectiles after a certain distance
+        if(Vector3.Distance(firingPoint, transform.position) > maxProjectileDistance)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
