fdd5f446121d3c1cab69619e222daf1d9e74ceeb
Joanna Oluoch <111391937+astronaut-nemo@users.noreply.github.com>
2022-12-03 02:34:44 +0300

    Updated UI
    -Fixed anchors on UI in Level Scene.
    -Made main menu and updated tutorial level.

---
+++ Head of the Herd/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject gameOverPanel;
+    public GameObject pauseGamePanel;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Time.timeScale = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    /* GAME STATES */
+
+    public void GameOver()
+    {
+        // Stop game from running in background
+        Time.timeScale = 0f;
+        gameOverPanel.SetActive(true);
+    }
+
+    public void PauseGame()
+    {
+        // Pause game from running in background
+        Time.timeScale = 0f;
+        pauseGamePanel.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        // Resume normal game time
+        Time.timeScale = 1.0f;
+        pauseGamePanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        // Quit the Game and close the application
+        Debug.Log("Quit Game!");
+        Application.Quit();
+    }
+
+    /* END OF GAME STATES*/
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+    /* LEVEL MANAGEMENT */
+
+    // Loads the specified scene
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+        Time.timeScale = 1.0f;
+    }
+    /* END OF LEVEL MANAGEMENT */
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+    /* SPAWN MANAGERS */
+}
